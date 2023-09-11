e158b1f6fff2932c0dae22c4733e5fe15cd3be90
Muzamil Khan <92785420+mkha101@users.noreply.github.com>
2023-09-11 19:14:11 -0400

    slideshow h1 text edited

--- src/components/Content-Graphic.tsx
+++ src/components/Content-Graphic.tsx
@@ -57,10 +57,11 @@ export const ContentGraphic = () => {
       </div>
       <div className="absolute flex flex-col justify-center items-center inset-0 bg-black bg-opacity-60">
         <div className=" flex sm:gap-10 gap-5 flex-col justify-center items-center ">
-          <h1 className="text-white text-2xl tracking-widest text-center font-extrabold xl:text-7xl lg:text-5xl">
-            Fall in love with your car <br /> all over again
+          <h1 className="text-white uppercase tracking-tighter text-3xl text-center font-black xl:text-7xl lg:text-5xl">
+            Fall in <span className="text-red-700">love</span> with your <br />
+            car all over again
           </h1>
-          <Button className="bg-transparent tracking-widest font-semibold text-xl hover:bg-transparent hover:border-red-700 transition duration-100 ease-in-out hover:text-red-700 border-[2px] rounded-full px-10 py-5  sm:px-20 sm:py-6 ">
+          <Button className="bg-transparent  font-semibold text-xl hover:bg-transparent hover:border-red-700 transition duration-100 ease-in-out hover:text-red-700 border-[2px] rounded-full px-10 py-5  sm:px-20 sm:py-6 ">
             Learn More
           </Button>
           <div className="flex flex-row absolute justify-centers bottom-12 gap-4">
