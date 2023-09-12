8007c684b95100c836779e828ea756d3aa15240a
Muzamil Khan <92785420+mkha101@users.noreply.github.com>
2023-09-12 02:38:44 -0400

    Testimonial section margin added for design

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -7,51 +7,73 @@ export const Testimonials = () => {
         <h1 className="text-xl text-center text-red-700 underline decoration-red-700 decoration-2 underline-offset-8">
           Reviews
         </h1>
-        <h1 className="text-3xl text-center text-black"></h1>
+        <h1 className="text-3xl text-center text-black absolute "></h1>
       </div>
       <div className="sm:grid grid-rows-2 grid-cols-3 flex py-20 sm:items-center sm:justify-center flex-wrap px-6   gap-10 flex-col sm:flex-row">
         {" "}
-        <div className="border relative px-2   bg-zinc-100 shadow-xl w-[20rem] sm:w-[20rem] h-72 rounded-xl">
+        <div className="border sm:mt-32 relative px-2 flex justify-center items-center text-black flex-col    bg-zinc-100 shadow-xl w-[20rem] sm:w-[20rem] h-72 rounded-xl">
           <img
             className="absolute top-4 left-3 w-10"
             src="/google.png"
             alt=""
           />
+          <h1>Reviewer</h1>
+          <p>Review Here</p>
         </div>
-        <div className="border relative px-2 bg-zinc-100 shadow-xl w-[18rem] sm:w-[20rem]  h-72 rounded-xl">
+        <div className="border relative flex justify-center items-center text-black flex-col  sm:mb-32 px-2 bg-zinc-100 shadow-xl w-[18rem] sm:w-[20rem]  h-72 rounded-xl">
           <img
             className="absolute top-4  left-3 w-10"
             src="/google.png"
             alt=""
           />
+          <div className="flex text-black absolute  justify-center items-center flex-col">
+            <h1>Reviewer</h1>
+            <p>Review Here</p>
+          </div>
         </div>
-        <div className="border relative px-2 bg-zinc-100 shadow-xl w-[16rem] sm:w-[20rem]  h-72 rounded-xl">
+        <div className="border sm:mt-32 flex justify-center items-center text-black flex-col  relative px-2 bg-zinc-100 shadow-xl w-[16rem] sm:w-[20rem]  h-72 rounded-xl">
           <img
             className="top-4 absolute left-3 w-10"
             src="/google.png"
             alt=""
           />
+          <div className="flex text-black absolute  justify-center items-center flex-col">
+            <h1>Reviewer</h1>
+            <p>Review Here</p>
+          </div>
         </div>
-        <div className="border relative px-2 bg-zinc-100 shadow-xl w-[14rem] sm:w-[20rem]  h-72 rounded-xl">
+        <div className="border flex justify-center items-center text-black flex-col   sm:mt-32 relative px-2 bg-zinc-100 shadow-xl w-[14rem] sm:w-[20rem]  h-72 rounded-xl">
           <img
             className="top-4 absolute left-3 w-10"
             src="/google.png"
             alt=""
           />
+          <div className="flex text-black absolute  justify-center items-center flex-col">
+            <h1>Reviewer</h1>
+            <p>Review Here</p>
+          </div>
         </div>
-        <div className="border hidden sm:block relative px-2 bg-zinc-100 shadow-xl w-[14rem] sm:w-[20rem]  h-72 rounded-xl">
+        <div className="border sm:flex justify-center items-center text-black flex-col  hidden sm:mb-32  relative px-2 bg-zinc-100 shadow-xl w-[14rem] sm:w-[20rem]  h-72 rounded-xl">
           <img
             className="top-4 absolute left-3 w-10"
             src="/google.png"
             alt=""
           />
+          <div className="flex text-black absolute  justify-center items-center flex-col">
+            <h1>Reviewer</h1>
+            <p>Review Here</p>
+          </div>
         </div>{" "}
-        <div className="border hidden sm:block  relative px-2 bg-zinc-100 shadow-xl w-[14rem] sm:w-[20rem]  h-72 rounded-xl">
+        <div className="border sm:flex justify-center items-center text-black flex-col  hidden sm:mt-32  relative px-2 bg-zinc-100 shadow-xl w-[14rem] sm:w-[20rem]  h-72 rounded-xl">
           <img
             className="top-4 absolute left-3 w-10"
             src="/google.png"
             alt=""
           />
+          <div className="flex text-black absolute  justify-center items-center flex-col">
+            <h1>Reviewer</h1>
+            <p>Review Here</p>
+          </div>
         </div>
       </div>
     </div>
