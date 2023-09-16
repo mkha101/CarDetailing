585033a6ae540683160918ce12ebc91995187151
Muzamil Khan <92785420+mkha101@users.noreply.github.com>
2023-09-15 21:05:52 -0400

    Added content to FAQ accordians

--- src/components/FrequentlyAskedQ.tsx
+++ src/components/FrequentlyAskedQ.tsx
@@ -15,21 +15,36 @@ export const FrequentlyAskedQ = () => {
       <h1 className=" text-2xl  pb-3  font-bold uppercase  leading-loose    text-black underline decoration-red-700 decoration-2 underline-offset-8">
         Frequently Asked <br /> Questions
       </h1>
-      <Accordion type="single" collapsible>
+      <Accordion className="flex flex-col gap-3" type="single" collapsible>
         <AccordionItem value="item-1">
-          <AccordionTrigger>Is it accessible?</AccordionTrigger>
+          <AccordionTrigger className="text-left">
+            What is car detailing, and how is it different from car washing?
+          </AccordionTrigger>
           <AccordionContent>
             Yes. It adheres to the WAI-ARIA design pattern.
           </AccordionContent>
         </AccordionItem>
-        <AccordionItem value="item-1">
-          <AccordionTrigger>Is it accessible?</AccordionTrigger>
+        <AccordionItem value="item-2">
+          <AccordionTrigger className="text-left">
+            How often should I get my car detailed?
+          </AccordionTrigger>
           <AccordionContent>
             Yes. It adheres to the WAI-ARIA design pattern.
           </AccordionContent>
         </AccordionItem>
-        <AccordionItem value="item-1">
-          <AccordionTrigger>Is it accessible?</AccordionTrigger>
+        <AccordionItem value="item-3">
+          <AccordionTrigger className="text-left">
+            How long does a typical car detailing session take?
+          </AccordionTrigger>
+          <AccordionContent>
+            Yes. It adheres to the WAI-ARIA design pattern.
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="item-4">
+          <AccordionTrigger className="text-left">
+            Do you offer mobile detailing services, or do I need to come to your
+            location?
+          </AccordionTrigger>
           <AccordionContent>
             Yes. It adheres to the WAI-ARIA design pattern.
           </AccordionContent>
