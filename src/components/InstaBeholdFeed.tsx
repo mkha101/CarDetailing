8b7dcad862211356b1cb5c04cb9903c17f0744ac
Muzamil Khan <92785420+mkha101@users.noreply.github.com>
2023-09-24 00:48:58 -0400

    reverted last commit

--- src/components/InstaBeholdFeed.tsx
+++ src/components/InstaBeholdFeed.tsx
@@ -7,10 +7,10 @@ export const InstaBeholdFeed = () => {
       {" "}
       <div className="p-6  sm:w-[40rem]  sm:h-[32rem] bg-zinc-900 rounded-lg">
         {" "}
-        <h1 className=" text-xl hidden sm:block  pb-3 text-center font-semibold uppercase  leading-loose    text-white underline decoration-blue-700 decoration-2 underline-offset-8">
+        <h1 className=" text-xl hidden sm:block  pb-3 sm:text-center font-semibold uppercase  leading-loose    text-white underline decoration-blue-700 decoration-2 underline-offset-8">
           Instagram Feed
         </h1>{" "}
-        <h1 className=" text-xl block sm:hidden pb-3 text-center font-semibold uppercase  leading-loose    text-white underline decoration-blue-700 decoration-2 underline-offset-8">
+        <h1 className=" text-xl block sm:hidden pb-3 sm:text-center font-semibold uppercase  leading-loose    text-white underline decoration-blue-700 decoration-2 underline-offset-8">
           Instagram <br /> Feed
         </h1>{" "}
         <figure className="pt-6" data-behold-id="GcWc7IDefG6DggI53Zqt"></figure>
