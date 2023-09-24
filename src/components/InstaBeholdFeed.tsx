fa2a1cc006f92089d07a151d3c606342b34b7f4c
Muzamil Khan <92785420+mkha101@users.noreply.github.com>
2023-09-23 22:04:43 -0400

    Added padding between Insta feed h1 and posts

--- src/components/InstaBeholdFeed.tsx
+++ src/components/InstaBeholdFeed.tsx
@@ -1,13 +1,14 @@
 import React from "react";
+import RevealOnScroll from "./RevealOnScroll";
 
 export const InstaBeholdFeed = () => {
   return (
-    <div className=" px-7 p-12 bg-black">
+    <div className=" px-7 p-12  bg-black">
       {" "}
       <h1 className=" text-xl  pb-3  font-semibold uppercase  leading-loose    text-white underline decoration-blue-700 decoration-2 underline-offset-8">
         Instagram Feed
       </h1>{" "}
-      <figure data-behold-id="GcWc7IDefG6DggI53Zqt"></figure>
+      <figure className="pt-6" data-behold-id="GcWc7IDefG6DggI53Zqt"></figure>
     </div>
   );
 };
