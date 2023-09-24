11068d1f2191f0c5198835ec58246d35bd6aad12
Muzamil Khan <92785420+mkha101@users.noreply.github.com>
2023-09-23 23:36:57 -0400

    Addes styling to instagram feed section

--- src/components/InstaBeholdFeed.tsx
+++ src/components/InstaBeholdFeed.tsx
@@ -3,12 +3,18 @@ import RevealOnScroll from "./RevealOnScroll";
 
 export const InstaBeholdFeed = () => {
   return (
-    <div className=" px-7 p-12  bg-black">
+    <div className=" px-7 py-10  bg-black">
       {" "}
-      <h1 className=" text-xl  pb-3 sm:text-center font-semibold uppercase  leading-loose    text-white underline decoration-blue-700 decoration-2 underline-offset-8">
-        Instagram Feed
-      </h1>{" "}
-      <figure className="pt-6" data-behold-id="GcWc7IDefG6DggI53Zqt"></figure>
+      <div className="p-6 bg-zinc-900 rounded-lg">
+        {" "}
+        <h1 className=" text-xl hidden sm:block  pb-3 sm:text-center font-semibold uppercase  leading-loose    text-white underline decoration-blue-700 decoration-2 underline-offset-8">
+          Instagram Feed
+        </h1>{" "}
+        <h1 className=" text-xl block sm:hidden pb-3 sm:text-center font-semibold uppercase  leading-loose    text-white underline decoration-blue-700 decoration-2 underline-offset-8">
+          Instagram <br /> Feed
+        </h1>{" "}
+        <figure className="pt-6" data-behold-id="GcWc7IDefG6DggI53Zqt"></figure>
+      </div>
     </div>
   );
 };
