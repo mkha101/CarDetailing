d2621ad79471f126c9f91f3bcf685fc21462e5b9
Muzamil Khan <92785420+mkha101@users.noreply.github.com>
2023-09-23 22:38:38 -0400

    gitignore

--- src/components/FrequentlyAskedQ.tsx
+++ src/components/FrequentlyAskedQ.tsx
@@ -6,55 +6,60 @@ import {
   AccordionTrigger,
 } from "@/components/ui/accordion";
 import { ScrollArea } from "@/components/ui/scroll-area";
+import Container from "./Container";
 
 export const FrequentlyAskedQ = () => {
   return (
-    <div className="px-7 bg-zinc-900  p-12">
-      <h1 className=" text-sm  py-3  font-semibold uppercase      text-white underline decoration-blue-700 decoration-2 underline-offset-8">
-        FAQ
-      </h1>
-      <h1 className=" text-2xl  pb-3  font-semibold uppercase  leading-loose    text-white underline decoration-blue-700 decoration-2 underline-offset-8">
-        Frequently Asked <br /> Questions
-      </h1>
-      <Accordion
-        className="flex flex-col text-white gap-4"
-        type="single"
-        collapsible
-      >
-        <AccordionItem value="item-1">
-          <AccordionTrigger className="text-left font-normal hover:no-underline">
-            What is car detailing, and how is it different from car washing?
-          </AccordionTrigger>
-          <AccordionContent>
-            Yes. It adheres to the WAI-ARIA design pattern.
-          </AccordionContent>
-        </AccordionItem>
-        <AccordionItem value="item-2">
-          <AccordionTrigger className="text-left font-normal hover:no-underline">
-            How often should I get my car detailed?
-          </AccordionTrigger>
-          <AccordionContent>
-            Yes. It adheres to the WAI-ARIA design pattern.
-          </AccordionContent>
-        </AccordionItem>
-        <AccordionItem value="item-3">
-          <AccordionTrigger className="text-left font-normal hover:no-underline">
-            How long does a typical car detailing session take?
-          </AccordionTrigger>
-          <AccordionContent>
-            Yes. It adheres to the WAI-ARIA design pattern.
-          </AccordionContent>
-        </AccordionItem>
-        <AccordionItem value="item-4">
-          <AccordionTrigger className="text-left font-normal hover:no-underline">
-            Do you offer mobile detailing services, or do I need to come to your
-            location?
-          </AccordionTrigger>
-          <AccordionContent>
-            Yes. It adheres to the WAI-ARIA design pattern.
-          </AccordionContent>
-        </AccordionItem>
-      </Accordion>
+    <div className=" bg-zinc-900   ">
+      <Container>
+        <div className="px-7   p-12">
+          <h1 className=" text-sm  py-3  font-semibold uppercase      text-white underline decoration-blue-700 decoration-2 underline-offset-8">
+            FAQ
+          </h1>
+          <h1 className=" text-2xl  pb-3  font-semibold uppercase  leading-loose    text-white underline decoration-blue-700 decoration-2 underline-offset-8">
+            Frequently Asked <br /> Questions
+          </h1>
+          <Accordion
+            className="flex flex-col text-white gap-4"
+            type="single"
+            collapsible
+          >
+            <AccordionItem value="item-1">
+              <AccordionTrigger className="text-left font-normal hover:no-underline">
+                What is car detailing, and how is it different from car washing?
+              </AccordionTrigger>
+              <AccordionContent>
+                Yes. It adheres to the WAI-ARIA design pattern.
+              </AccordionContent>
+            </AccordionItem>
+            <AccordionItem value="item-2">
+              <AccordionTrigger className="text-left font-normal hover:no-underline">
+                How often should I get my car detailed?
+              </AccordionTrigger>
+              <AccordionContent>
+                Yes. It adheres to the WAI-ARIA design pattern.
+              </AccordionContent>
+            </AccordionItem>
+            <AccordionItem value="item-3">
+              <AccordionTrigger className="text-left font-normal hover:no-underline">
+                How long does a typical car detailing session take?
+              </AccordionTrigger>
+              <AccordionContent>
+                Yes. It adheres to the WAI-ARIA design pattern.
+              </AccordionContent>
+            </AccordionItem>
+            <AccordionItem value="item-4">
+              <AccordionTrigger className="text-left font-normal hover:no-underline">
+                Do you offer mobile detailing services, or do I need to come to
+                your location?
+              </AccordionTrigger>
+              <AccordionContent>
+                Yes. It adheres to the WAI-ARIA design pattern.
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
+        </div>
+      </Container>{" "}
     </div>
   );
 };
--- src/components/InstaBeholdFeed.tsx
+++ src/components/InstaBeholdFeed.tsx
@@ -5,7 +5,7 @@ export const InstaBeholdFeed = () => {
   return (
     <div className=" px-7 p-12  bg-black">
       {" "}
-      <h1 className=" text-xl  pb-3  font-semibold uppercase  leading-loose    text-white underline decoration-blue-700 decoration-2 underline-offset-8">
+      <h1 className=" text-xl  pb-3 sm:text-center font-semibold uppercase  leading-loose    text-white underline decoration-blue-700 decoration-2 underline-offset-8">
         Instagram Feed
       </h1>{" "}
       <figure className="pt-6" data-behold-id="GcWc7IDefG6DggI53Zqt"></figure>
