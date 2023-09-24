c46f436cecb0e6870625d80dd16a22926a26f785
Muzamil Khan <92785420+mkha101@users.noreply.github.com>
2023-09-24 04:13:04 -0400

    Started portfolio page

--- src/components/PortfolioBanner.tsx
+++ src/components/PortfolioBanner.tsx
@@ -8,13 +8,13 @@ import React, { useEffect, useState } from "react";
 export const PortfolioBanner = () => {
   return (
     <div className="sm:min-h-screen min-h-[82vh] z-0 relative">
-      <div className="absolute flex flex-col justify-start pt-20 items-center inset-0 bg-zinc-700 ">
-        <div className=" flex  sm:gap-10 gap-16 mt-12 sm:px-4 px-3  flex-col justify-center items-center ">
-          <h1 className="text-white drop-shadow-2xl leading-tight uppercase  tracking-tight text-3xl text-center font-semibold xl:text-5xl lg:text-5xl">
-            Portfolio
-          </h1>
-          <div className="flex flex-row absolute justify-centers bottom-5 gap-2"></div>
-        </div>
+      <div className="absolute flex flex-col justify-start gap-3 pt-56 px-24 items-start inset-0 bg-black ">
+        <p className="text-base drop-shadow-2xl uppercase  tracking-widest  text-white font-normal">
+          Portfolio
+        </p>
+        <h1 className="text-white drop-shadow-2xl leading-tight uppercase  tracking-widest text-3xl text-center font-semibold xl:text-7xl lg:text-5xl">
+          Detailing Portfolio
+        </h1>
       </div>
     </div>
   );
